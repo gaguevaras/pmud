6d1900d9f99679654245ee713d2b2918c7fb794f
Gustavo Guevara <gustavo.andres.guevara@gmail.com>
2019-03-14 09:56:54 -0500

    Create a structure to save output observers.

--- pmud/mud.py
+++ pmud/mud.py
@@ -12,13 +12,23 @@ class Mud:
     def __init__(self, telnet):
         self.tn = telnet
         self.__input_observers = []
+        self.__output_observers = []
 
     def register_input_observer(self, observer):
         self.__input_observers.append(observer)
 
+    def register_output_observer(self, observer):
+        self.__output_observers.append(observer)
+
     def notify_input_observers(self, *args, **kwargs):
         responses = []
         for observer in self.__input_observers:
+            responses.append(observer.notify(self, *args, **kwargs))
+        return responses
+
+    def notify_output_observers(self, *args, **kwargs):
+        responses = []
+        for observer in self.__output_observers:
             responses.append(observer.notify(self, *args, **kwargs))
         return responses
 
@@ -34,7 +44,7 @@ class Mud:
                 for key, events in selector.select():
                     if key.fileobj is self.tn:
                         try:
-                            text = self.tn.read_eager()
+                            text = self.tn.read_very_eager()
                         except EOFError:
                             print('*** Connection closed by remote host ***')
                             return
@@ -54,6 +64,7 @@ class Mud:
 
     def receive(self, text):
         # Determine if text triggers any actions
+        
         sys.stdout.write(text.decode('ascii'))
         sys.stdout.flush()
 
